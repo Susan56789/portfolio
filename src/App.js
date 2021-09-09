0137038235b5644a242893dae7895bf72273c263
Susan56789 <mwihaki2susan@gmail.com>
2021-09-09 21:10:45 +0300

    portfolio

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Testimonials from "./components/Testimonials";
-import CopyrightYear from 'react-CopyrightYear';
+import Copyright from 'react-copyright';
 
 export default function App() {
   return (
@@ -16,7 +16,7 @@ export default function App() {
       <Skills />
       <Testimonials />
       <Contact />
-      <CopyrightYear/>
+      <Copyright/>
     </main>
   );
 }

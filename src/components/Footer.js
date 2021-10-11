68e764460c4b36b04c1519b4dd070aac726d8b8b
Susan Wairimu <mwihaki2susan@gmail.com>
2021-10-11 10:05:47 +0300

    made changes to social icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -22,13 +22,6 @@ return(
         </Col>
     </Row>
 </Container>
-<Container>
-    <Row>
-        <Col>
-        <p> Copyright &copy;  2021.  All Rights Reserved</p>
-        </Col>
-    </Row>
-</Container>
 </footer>
 );
 }
